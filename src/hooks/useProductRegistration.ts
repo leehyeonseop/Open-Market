e889684cc1d500ba7ca6a6d5cdcf00152857cd2b
leehyeonseop <gustjq4146@naver.com>
2022-10-31 16:50:28 +0900

    상품 등록시 토큰 null 에러 해결

--- src/hooks/useProductRegistration.ts
+++ src/hooks/useProductRegistration.ts
@@ -4,28 +4,28 @@ import { useMutation } from 'react-query';
 import { axiosInstance, getJWTHeader } from '../axiosInstance';
 import { getUser } from '../localStorage';
 
-const user = getUser();
-
-const productRegistration = async (data: FieldValues) => {
-    const reqData = {
-        product_name: data.name,
-        image: data.image,
-        price: data.price,
-        shipping_method: data.shippingMethod, // PARCEL 또는 DELIVERY 선택
-        shipping_fee: data.deliveryFee,
-        stock: data.stock,
-        product_info: data.description,
-    };
-    await axiosInstance.post('products/', reqData, {
-        headers: {
-            ...getJWTHeader(user),
-            'Content-Type': 'multipart/form-data',
-        },
-    });
-};
-
 export const useProductRegistration = () => {
+    const user = getUser();
     const [open, setOpen] = useState(false);
+
+    const productRegistration = async (data: FieldValues) => {
+        const reqData = {
+            product_name: data.name,
+            image: data.image,
+            price: data.price,
+            shipping_method: data.shippingMethod, // PARCEL 또는 DELIVERY 선택
+            shipping_fee: data.deliveryFee,
+            stock: data.stock,
+            product_info: data.description,
+        };
+        await axiosInstance.post('products/', reqData, {
+            headers: {
+                ...getJWTHeader(user),
+                'Content-Type': 'multipart/form-data',
+            },
+        });
+    };
+
     const { mutate: productRegister } = useMutation(
         (data: FieldValues) => productRegistration(data),
         {
